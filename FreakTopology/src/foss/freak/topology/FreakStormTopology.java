4e1dd7a04062dcb78e30dde87d1f11e7c3e4e5bc
ANUBHAV GULERIA <anubhav.nitsri.it@gmail.com>
2016-08-08 20:07:22 +0530

    Update FreakStormTopology.java

--- FreakTopology/src/foss/freak/topology/FreakStormTopology.java
+++ FreakTopology/src/foss/freak/topology/FreakStormTopology.java
@@ -1,10 +1,12 @@
 package foss.freak.topology;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
-import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.generated.Bolt;
+import org.apache.storm.topology.TopologyBuilder;import org.apache.storm.trident.operation.DefaultResourceDeclarer;
 
 import foss.freak.bolt.FreakEvenBolt;
 import foss.freak.bolt.FreakFilterBolt;
@@ -29,24 +31,30 @@ public class FreakStormTopology {
 		
 		config.setDebug(true);
 		config.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
-		
-		
+		config.put(Config.TOPOLOGY_DEBUG,false);
 		
 		TopologyBuilder builder = new TopologyBuilder();		
-		builder.setSpout("readerEven", new FreakEvenNumberSpout());
+		/*
+		 * ;;;;;;;;;;Working;;;;
+		 * builder.setSpout("readerEven", new FreakEvenNumberSpout());
 		builder.setSpout("readerOdd", new FreakOddNumberSpout());
 		builder.setBolt("even_bolt", new FreakEvenBolt()).shuffleGrouping("readerEven");
 		builder.setBolt("odd_bolt", new FreakOddBolt()).shuffleGrouping("readerOdd");
 		builder.setBolt("sink", new FreakSink()).shuffleGrouping("even_bolt").shuffleGrouping("odd_bolt");
-		//builder.setBolt("root_bolt", new FreakFilterBolt(),2).shuffleGrouping("readerEven").shuffleGrouping("readerOdd");
+		Working*/
 		
-		//builder.setBolt("even_bolt", new FreakEvenBolt()).shuffleGrouping("root_bolt");
-		//builder.setBolt("odd_bolt", new FreakOddBolt(),20).shuffleGrouping("root_bolt");
-		/*builder.setBolt("sink", new FreakSink()).shuffleGrouping("even_bolt").shuffleGrouping("odd_bolt");*/
+		builder.setSpout("readerEven", new FreakEvenNumberSpout());
+		builder.setSpout("readerOdd", new FreakOddNumberSpout());
+		builder.setBolt("root_bolt", new FreakFilterBolt(),2).shuffleGrouping("readerEven").shuffleGrouping("readerOdd");
+		
+		builder.setBolt("even_bolt", new FreakEvenBolt()).shuffleGrouping("root_bolt","EvenStream");
+		builder.setBolt("odd_bolt", new FreakOddBolt(),20).shuffleGrouping("root_bolt","OddStream");
+		builder.setBolt("sink", new FreakSink()).shuffleGrouping("even_bolt").shuffleGrouping("odd_bolt");
 		
 		
 		
 		LocalCluster cluster = new LocalCluster();
+		
 		cluster.submitTopology("FreakStormTopology", config, builder.createTopology());
 		
 		Thread.sleep(100000);
